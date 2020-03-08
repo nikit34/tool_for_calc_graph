d6babd7a254c97e5e130604ab36b6bf6c0bc48f4
Никита Пермяков <38742537+nikit34@users.noreply.github.com>
2020-03-08 11:02:33 +0300

    dont working

--- main.js
+++ main.js
@@ -15,6 +15,16 @@ class MainPage {
     F = new Field();
     return F.object_inside.length;
   }
+
+  get select_node() {
+    return document.getElementsByClassName('mark')[0];
+  }
+
+  getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
 }
 
 class TopSetting extends MainPage {
@@ -221,12 +231,10 @@ I = new Interaction();
 I.event_listens();
 
 
-class PopUp {
-  constructor() {};
-
-  select_node() {
-    return document.getElementsByClassName('mark')[0];
-  }
+class PopUp extends MainPage{
+  constructor() {
+    super();
+  };
 
   select_popup_body() {
     return document.getElementById('popup_body');
@@ -236,62 +244,125 @@ class PopUp {
     return document.getElementById('popup_input');
   }
 
-  check_call_popup() {
-    PU = new PopUp()
+  select_popup_button() {
+    return document.getElementById('popup_button');
+  }
+
+  processing_popup() {
     document.body.addEventListener("dblclick", function(e) {
-      if (e.target.classList.contains("field") && PU.select_popup_body() == null) {
-        e.preventDefault();
-        let one_node = e.target;
-        one_node.setAttribute("class", "node draggable field mark");
-        PU.create_popup();
-      } else if (PU.select_popup_body() && PU.select_popup_input()) {
-        if ((e.target.id == "popup_body" || e.target.id == "popup_button") && PU.select_popup_input().value) {
-          PU.save_data()
-          PU.remove_popup(false);
-        } else if ((e.target.id == "popup_body" || e.target.id == "popup_button") && !(PU.select_popup_input().value)){
-          PU.select_popup_body().style.backgroundColor = "rgba(255,50,50,.5)";
-        } else {
-          PU.remove_popup(true);
-        }
-      } else if (e.target.id != "popup_body"){
-          // TODO: hidden popup
-      } else {
-        // TODO: show popup
-      }
+      PU.double_click_proc(e);
     });
     document.body.addEventListener("mouseup", function(e) {
-      if (e.target.classList.contains("field")) {
-        e.preventDefault();
-        let one_node = e.target;
-        one_node.setAttribute("class", "node draggable field");
-      }
+      PU.mouse_up_proc(e);
     });
+  }
+
+  double_click_proc(e) {
+    // create popup if (ELEM at field) and (BODY) popups NOT EXIST
+    if (e.target.classList.contains("field") && PU.select_popup_body() == null) {
+      e.preventDefault();
+      let one_node = e.target;
+      one_node.setAttribute("class", "node draggable field mark");
+      PU.create_popup();
+    // else if (BODY) and (INPUT) popups EXIST
+    } else if (PU.select_popup_body() && PU.select_popup_input()) {
+      // save data and remove popup if MOUSE FOCUSES on (BUTTON) and (VALUE) EXIST
+      if ((e.target.id == "popup_button") && PU.select_popup_input().value) {
+        let value = PU.select_popup_input().value;
+        PU.save_data(value);
+        PU.remove_popup(false);
+        PU.display_popup(value);
+        PU.change_state_node();
+      // change color if MOUSE FOCUSES on ((BUTTON or BODY) and (VALUE)) NOT EXIST
+      } else if ((e.target.id == "popup_body" || e.target.id == "popup_button") && !(PU.select_popup_input().value)){
+        PU.select_popup_body().style.backgroundColor = "rgba(255,50,50,0.5)";
+      } else {
+      // remove popup completely
+        PU.remove_popup(true);
+      }
+    // else if only (BODY) popups EXIST
+    } else if (PU.select_popup_body() && !PU.select_popup_input()) {
+      // if MOUSE FOCUSES on (BODY)
+      if (e.target.id == "popup_body"){
+        e.target.parentElement.setAttribute("class", "node draggable field mark");
+        PU.remove_popup(true);
+        PU.create_popup();
+      // if MOUSE NOT FOCUSES on (BODY)
+      } else {
+        PU.remove_popup(true);
+      }
+    }
+  }
+
+  mouse_up_proc(e) {
+    if (e.target.classList.contains("field")) {
+      e.preventDefault();
+      let one_node = e.target;
+      one_node.textContent = "W";
+      one_node.setAttribute("class", "node draggable field");
+    }
   }
 
   create_popup() {
     let build_start = PU.select_node();
     let popup_body = document.createElement("DIV");
     popup_body.setAttribute("id", "popup_body");
-    build_start.after(popup_body);
+    build_start.appendChild(popup_body);
     popup_body.style.position = "absolute";
-    popup_body.style.top = (parseFloat(build_start.style.top) - parseFloat(popup_body.offsetHeight) - 10.0).toString() + "px";
-    popup_body.style.left = (parseFloat(build_start.style.left) + parseFloat(build_start.offsetWidth) / 2 - parseFloat(popup_body.offsetWidth) / 2).toString() + "px";
+    popup_body.style.top = (- parseFloat(popup_body.offsetHeight) - 10.0).toString() + "px";
+    popup_body.style.left = (parseFloat(build_start.offsetWidth) / 2 - parseFloat(popup_body.offsetWidth) / 2 - 5).toString() + "px";
     let popup_input = document.createElement("INPUT");
     popup_input.setAttribute("id", "popup_input");
     popup_body.appendChild(popup_input);
     popup_input.style.position = "absolute";
     popup_input.style.top = "10px";
     popup_input.style.left = "10px";
+    let popup_button = document.createElement("BUTTON");
+    popup_button.setAttribute("id", "popup_button");
+    popup_body.appendChild(popup_button);
+    popup_button.innerHTML = "save";
+    popup_button.style.position = "absolute";
+    popup_button.style.top = "10px";
+    popup_button.style.left = "70px";
+  }
+
+  display_popup(value) {
+    let popup_body = PU.select_popup_body();
+    popup_body.style.width = "60px";
+    popup_body.style.height = "30px";
+    popup_body.style.top = (parseFloat(popup_body.style.top) + 10.0).toString() + "px";
+    popup_body.style.left = (parseFloat(popup_body.style.left) + 30.0).toString() + "px";
+    for(let key of Object.keys(localStorage)) {
+      if (localStorage.getItem(key).toString() == value.toString()){
+        popup_body.textContent = "key: " + key;
+      }
+    }
+    // popup_body.parentElement.textContent = value.toString();
   }
 
   remove_popup(without_width) {
-    (without_width) ? PU.select_popup_body().remove() : PU.select_popup_input().remove();
+    if (without_width) {
+       PU.select_popup_body().remove();
+    } else {
+      PU.select_popup_input().remove();
+      PU.select_popup_button().remove();
+    }
   }
 
-  save_data() {
-    
+  save_data(value) {
+    this.random_num = [];
+    let random_value = this.getRandomInt(0, 100);
+    while (this.random_num.includes(random_value)) {
+      random_value = this.getRandomInt(0, 100);
+    }
+    this.random_num[this.random_num.length] = random_value
+    localStorage.setItem(this.random_num[this.random_num.length - 1].toString(), value.toString());
+  }
+
+  change_state_node() {
+    PU.select_node().setAttribute("class", "node draggable field mark save");
   }
 }
 
 PU = new PopUp();
-PU.check_call_popup();
+PU.processing_popup();
